c093f229480f30ec5ae72caf50558ea078a160d8
Werwolfx <aasdvv@gmail.com>
2024-12-11 22:47:55 +0300

    fix deprecated next image

--- next.config.js
+++ next.config.js
@@ -1,24 +1,57 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- //output: 'export',
- output: "standalone",
+  // output: 'export',
+  output: "standalone",
   images: {
-    //unoptimized: true,
-    domains: [
-      "xn-----6kccstfpfjmicbdprf7c7cl0k6bjc.xn--p1ai",
-      "images.pexels.com",
-      "img.freepik.com",
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-      "i.ibb.co",
-      "flaticon.com",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xn-----6kccstfpfjmicbdprf7c7cl0k6bjc.xn--p1ai',
+        port: '',
+        pathname: '/**', // Разрешает доступ ко всем путям
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flaticon.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
 
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(nextConfig);
