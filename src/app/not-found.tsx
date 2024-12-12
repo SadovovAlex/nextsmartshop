f183d637d2043e64d71bf3ffdf39e1f87eb960f9
Werwolfx <aasdvv@gmail.com>
2024-12-13 00:02:23 +0300

    optimize img

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,26 @@
 import Container from "@/components/Container";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image"; 
+
+import Head from "next/head";
 
 const NotFoundPage = () => {
   return (
     <Container className="flex items-center justify-center py-20 bg-gray-100">
+      <Head>
+        <title>404 - Страница не найдена</title>
+        <meta name="description" content="Страница не найдена. Вернитесь на главную." />
+      </Head>
       <div className="max-w-2xl min-h-[400px] flex flex-col items-center justify-center gap-y-5 bg-white shadow-lg rounded-lg p-8">
         <h2 className="text-4xl font-bold text-red-600">Страница не найдена</h2>
         
-        <img
-          src="/static/404.png" // Путь к изображению 404 (добавьте свое изображение)
+        <Image
+          src="/static/404.png"
           alt="404 Not Found"
-          className="w-1/2 h-auto my-4"
+          className="w-full max-w-xs h-auto my-4" // Адаптивный размер изображения
+          width={500} // Укажите ширину изображения
+          height={300} // Укажите высоту изображения
         />
         <Link
           href={"/"}
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Logo = () => {
   return (
     <Link href={"/"}>
       <div className="flex items-center cursor-pointer duration-200">
-        <img
-          src="/static/logo.png" // Путь к вашему изображению
+        <Image
+          src="/static/logo.png"
           alt="Логотип"
           className="w-16 h-16 transition-transform duration-200 transform hover:scale-110"
+          width={500} 
+          height={300} 
         />
-       <h3 className="text-base font-semibold hover:text-orange-500 transition-transform duration-200 transform hover:translate-y-[-2px]">
-  Ферма Шуваловых
-</h3>
+
+        <h3 className="text-base font-semibold hover:text-orange-500 transition-transform duration-200 transform hover:translate-y-[-2px]">
+          Ферма Шуваловых
+        </h3>
 
       </div>
     </Link>
