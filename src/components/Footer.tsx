ac10496b93d3b0cdeaf662962904f61802ddc2c9
Werwolfx <aasdvv@gmail.com>
2025-01-03 20:25:15 +0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,45 +29,39 @@ const Footer = () => {
             <span className="font-semibold">Дни доставки:</span> пн, вт, чт, пт, сб.
           </p>
 
+        </div>
+
+        <div>
           {/* Phone Number */}
           <div className="items-center text-sm font-semibold text-white">
-            <p>Телефон: 8-995-963-00-40</p>
+            <p>Телефон для заказа: 8-995-963-00-40</p>
+            <p>Email: evgenii.sidorov19872801@gmail.com</p>
           </div>
 
-          {/* Email Address */}
-          <div className="flex items-center text-sm font-semibold text-white">
-            <p className="mr-2">Email:</p>
-            <p>evgenii.sidorov19872801@gmail.com</p>
+          {/* Contact Information */}
+          <div className="flex flex-col text-sm font-semibold text-white mt-2">
+
+            {/* Address */}
+            <div className="flex items-center mt-1">
+              <p className="mr-2">Адрес:</p>
+              <a
+                href="https://maps.google.com/maps/search/%D0%9A%D0%BE%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%B0%D1%8F%20%D1%83%D0%BB.%2C%2070%2C%20%D0%97%D0%B0%D0%BE%D0%BA%D1%81%D0%BA%D0%BE%D0%B5%2C%20%D0%A0%D1%8F%D0%B7%D0%B0%D0%BD%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB.%2C%20%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C%20390019/@54.6898,39.7476,17z?hl=en"
+                className="text-blue-400 hover:underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Кооперативная ул., 70, Заокское, Рязанская обл., Россия, 390019
+              </a>
+            </div>
+
+            {/* Working Hours */}
+            <div className="flex items-center mt-1">
+              <p className="mr-2">Часы работы:</p>
+              <p>9:00 - 18:00</p>
+            </div>
           </div>
-          
-         {/* Contact Information */}
-<div className="flex flex-col text-sm font-semibold text-white mt-2">
 
-  {/* Address */}
-  <div className="flex items-center mt-1">
-    <p className="mr-2">Адрес:</p>
-    <a 
-      href="https://maps.google.com/maps/search/%D0%9A%D0%BE%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%B0%D1%8F%20%D1%83%D0%BB.%2C%2070%2C%20%D0%97%D0%B0%D0%BE%D0%BA%D1%81%D0%BA%D0%BE%D0%B5%2C%20%D0%A0%D1%8F%D0%B7%D0%B0%D0%BD%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB.%2C%20%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C%20390019/@54.6898,39.7476,17z?hl=en" 
-      className="text-blue-400 hover:underline"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Кооперативная ул., 70, Заокское, Рязанская обл., Россия, 390019
-    </a>
-  </div>
-
-  {/* Working Hours */}
-  <div className="flex items-center mt-1">
-    <p className="mr-2">Часы работы:</p>
-    <p>9:00 - 18:00</p>
-  </div>
-</div>
-
-
-
-
-
-          <div className="flex items-center gap-x-4">
+          <div className="socialLink flex items-center gap-x-4">
             <a href="#" target="_blank">
               <span className="socialLink">
                 <BsInstagram />
@@ -95,25 +89,8 @@ const Footer = () => {
             </a>
           </div>
         </div>
-        <div>
-          <p className="text-lg">События</p>
-          <ul className="text-sm font-light mt-2 flex flex-col gap-y-2">
-            <li className="flex flex-col">
-              <span className="text-slate hover:text-orange-600 cursor-pointer duration-200">
-                Молочный фестиваль 2024
-              </span>
-              <span className="text-orange-600">Август 15, 2024</span>
-            </li>
-            <li className="flex flex-col">
-              <span className="text-slate hover:text-orange-600 cursor-pointer duration-200">
-                Вебинар: Польза молока для здоровья
-              </span>
-              <span className="text-orange-600">Сентябрь 10, 2024</span>
-            </li>
-          </ul>
-        </div>
 
-        <div>
+        <div className="links">
           <p className="text-lg">Ссылки</p>
           <ul className="text-base font-medium mt-2 flex flex-col gap-y-2">
             <Link href={"/"}>
