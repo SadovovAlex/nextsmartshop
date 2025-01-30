57be1ac11f3057f83b7c12d9eeb3b8e787fee0d2
Werwolfx <aasdvv@gmail.com>
2025-01-30 22:48:34 +0300

    add img + change 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,24 @@
 import Container from "@/components/Container";
 import Link from "next/link";
 import React from "react";
-import Image from "next/image"; 
-import Menu from "@/components/Menu"; 
-import Head from "next/head";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+
+// Определяем метаданные
+export const metadata = {
+  title: "404 - Страница не найдена",
+  description: "Страница не найдена. Вернитесь на главную.",
+};
 
 const NotFoundPage = () => {
   return (
-    <Container className="flex items-center justify-center py-20 bg-gray-100">
-      <Head>
-        <title>404 - Страница не найдена</title>
-        <meta name="description" content="Страница не найдена. Вернитесь на главную." />
-      </Head>
-      <div className="max-w-2xl min-h-[400px] flex flex-col items-center justify-center gap-y-5 bg-white shadow-lg rounded-lg p-8">
-<Menu/>
-        <h2 className="text-4xl font-bold text-red-600">Страница не найдена</h2>
+    <div>
+    <Menu/> 
+    <Container>
+      <div className="min-h-[500px] flex flex-col items-center justify-center gap-y-5 bg-white shadow-lg rounded-lg p-8">
         
+        <h2 className="text-4xl font-bold text-red-600">Страница не найдена</h2>
+
         <Image
           src="/static/404.webp"
           alt="404 Not Found"
@@ -23,9 +26,9 @@ const NotFoundPage = () => {
           width={500} // Укажите ширину изображения
           height={300} // Укажите высоту изображения
         />
-       
       </div>
     </Container>
+    </div> 
   );
 };
 
